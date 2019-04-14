9569f5590b369f4fd4ad0f254635e58cdbe0852b
ComputerNerd2003 <49621454+ComputerNerd2003@users.noreply.github.com>
2019-04-14 16:42:14 -0300

    Update dorktool.py

--- dorktool.py
+++ dorktool.py
@@ -1,13 +1,19 @@
+#Prints a nice little splash screen.
 print "==================="
 print "=---PlagueHax-----="
 print "=--2019/04/14-----="
 print "==================="
 print "Used for searching Google dorks."
-
+#To search.
 from googlesearch import search
 ip=raw_input("Dork: ")
+#Fakes a user agent to prevent Google from flagging your connection as a botnet.
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+#Finds and prints a set number of results.
 for url in search(ip, stop=15):
     print(url)
+
+
+
 
 
